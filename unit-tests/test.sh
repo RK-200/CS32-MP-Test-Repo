f1de28b9161da2a15bbf5362ecbef326aac396d8
anjelo fan <gpfan@upd.edu.ph>
2025-01-08 20:29:49 +0800

    added outputs to test script and multiple testing

--- unit-tests/test.sh
+++ unit-tests/test.sh
@@ -8,20 +8,30 @@ st=../seq-of-trees/$name
 sl=../skip-list/$name 
 py=/test.py
 
-declare -a arr=( $st )
+
+declare -a names=("stree" "sl" "dll")
+
+declare -a arr=( $st $sl $dll )
 
 name_py=main.py
 
 tc=$1
-
+testfile=$tc.txt
 
 gcc -o ./tmp/test $dll
-./test < $tc.txt > ./tmp/out.a
+./tmp/test < $testfile > ./tmp/out.a
 
-for i in "${arr[*]}"
+for i in $(seq 0 2)
 do
-gcc -o ./tmp/test2 $i
-./test2 < $tc.txt > ./tmp/out.b
-diff ./tmp/out.b ./tmp/out.a
+echo ${names[$i]}
+gcc -o ./tmp/test2${names[$i]} ${arr[$i]}
+./tmp/test2${names[$i]} < $testfile > ./tmp/out.b
+
+if diff ./tmp/out.b ./tmp/out.a; then
+echo "Output correct"
+else
+echo "Output incorrect!"
+fi
+
 done
 
